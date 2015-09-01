61c53a8a1638b1c46ec2a39fbcec4f331077deea
Gerardo Cordero <cordero.gerard@gmail.com>
2015-09-01 15:54:06 -0430

    Iniciendo el blog con el Framework Spark de Java, Maven, MongoDB y deployment con Heroku

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+/**
+ * Created by gerardo on 01/09/15.
+ */
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello World");
+
+    }
+
+    private static int getHerokuAssignedPort() {
+
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        if(processBuilder.environment().get("PORT")!= null){
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+
+    }
+}
